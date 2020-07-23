805174027b970fe3af32a46f59026345f1db5ce2
Yogi Putra <mail.yogiputra@gmail.com>
2020-07-23 14:48:14 +0700

    add login

--- app/src/main/java/com/yogi/stockbit/MainActivity.kt
+++ app/src/main/java/com/yogi/stockbit/MainActivity.kt
@@ -2,15 +2,10 @@ package com.yogi.stockbit
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         val navController = nav_host_fragment.findNavController()
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
